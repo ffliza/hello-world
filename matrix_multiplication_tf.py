757165c2d470b2cb9a88a8fde5fe106100340dd8
Farhana Ferdousi Liza <fferdousi.liza@gmail.com>
2018-01-09 23:03:01 +0000

    Update matrix_multiplication_tf.py

--- matrix_multiplication_tf.py
+++ matrix_multiplication_tf.py
@@ -14,9 +14,12 @@ import tensorflow as tf
 
 
 #define placeholder
-
 x = tf.placeholder(tf.float32, [3, 4])
 w = tf.placeholder(tf.float32, [4, 5])
+
+# add an op to initialize the variables
+init_op = tf.global_variables_initializer()
+
 
 # define model
 mul = tf.matmul(x,w)
@@ -27,7 +30,9 @@ w_rand_normal = np.random.normal(0.0,0.5, (4,5))
 
 # session starts 
 sess = tf.Session()
+k = sess.run(init_op)
 y = sess.run(mul, feed_dict={x:x_rand_normal, w:w_rand_normal})
 
 # print result
-print(y, type(y)) 
+print(k)
+print(y, type(y))  
